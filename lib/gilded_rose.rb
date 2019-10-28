cd97d54d4d046e4c9246ccb290ea245b296652ca
Tom Spencer <tom@homeflow.co.uk>
2019-10-28 22:20:08 +0000

    Move classes into GildedRose module.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,3 @@
-require 'normal'
-require 'brie'
-require 'item'
-require 'backstage'
-require 'brie'
-require 'backstage'
-# require 'normal'
-
 require 'yaml'
 
 module GildedRose
@@ -14,10 +6,53 @@ module GildedRose
   end
 
   def self.klass(name)
-    klasses['specialised'].fetch(name, 'Item')
+    klasses['specialised'].fetch(name, 'GildedRose::Item')
   end
 
   def self.klasses
     YAML::load_file(File.join(__dir__, 'items.yml'))
+  end
+
+  class Item
+    attr_reader :quality, :days_remaining
+  
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+  
+    def tick
+    end
+  end
+
+  class Normal < Item
+
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+      @quality -= 1
+      @quality -= 1 if @days_remaining < 0
+    end
+  end
+
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+  
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+
+  class Backstage < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+
+      @quality += 1
+      @quality += 1 if @days_remaining < 10
+      @quality += 1 if @days_remaining < 5
+      @quality = 0 if @days_remaining < 0
+    end
   end
 end
